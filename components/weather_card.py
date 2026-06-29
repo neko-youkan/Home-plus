9d808720f2cbcb5d4629e9760c73ccbd167e0bea
neko-youkan <tokei.uhr@gmail.com>
2026-06-29 23:30:50 +0900

    Update UI design

--- components/weather_card.py
+++ components/weather_card.py
@@ -1,11 +1,13 @@
 import streamlit as st
 
 from components.ui import show_title
-from services.weather_service import get_weather
+from services.weather_service import get_weather, weather_icon
 
 
 def show_weather_card():
     weather = get_weather()
+
+    icon = weather_icon(weather["weather"])
 
     with st.container(border=True):
         show_title(
@@ -20,20 +22,22 @@ def show_weather_card():
         with left:
             st.markdown(
                 f"""
-<div class="weather-main">
-    <span class="weather-icon">☀️</span>
-    <span class="weather-text">{weather['weather']}</span>
-</div>
-""",
+                <div class="weather-main">
+                    <span class="weather-icon">{icon}</span>
+                    <span class="weather-text">{weather["weather"]}</span>
+                </div>
+                """,
                 unsafe_allow_html=True,
             )
 
         with right:
             st.markdown(
                 f"""
-        <div class="weather-temp">
-            {weather['temperature']}
-        </div>
-        """,
+                <div class="weather-right">
+                    <div class="weather-temp">
+                        {weather["temperature"]}
+                    </div>
+                </div>
+                """,
                 unsafe_allow_html=True,
             )
